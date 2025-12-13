b122de214c0eef77cd262f4573a97b95456ddf58
goalgoda <goalgoda@users.noreply.github.com>
2025-12-14 00:52:32 +0900

    refactor: CSS/JS/HTMLファイル整理・統合
    - 全CSSを assets/css/main.css に統合（6,997行）
    - businesspress JSを assets/js/main.js に統合
    - particles.js/particlesjs-config.js を assets/js/ に移動
    - Font Awesome を assets/fonts/ に移動
    - 全HTMLファイルのCSS/JS参照を更新（17ファイル）
    - HTTPリクエスト削減: CSS 6→1, JS 多数→4
    - jQuery を CDN経由に変更（3.7.1）
    - 不要なWordPressプラグインJS削除
    - コンタクトフォーム入力フィールドの幅修正

---
+++ assets/js/main.js
@@ -0,0 +1,221 @@
+/*jshint browser:true */
+/*!
+* FitVids 1.1
+*
+* Copyright 2013, Chris Coyier - http://css-tricks.com + Dave Rupert - http://daverupert.com
+* Credit to Thierry Koblentz - http://www.alistapart.com/articles/creating-intrinsic-ratios-for-video/
+* Released under the WTFPL license - http://sam.zoy.org/wtfpl/
+*
+*/
+
+;(function( $ ){
+
+  'use strict';
+
+  $.fn.fitVids = function( options ) {
+    var settings = {
+      customSelector: null,
+      ignore: null
+    };
+
+    if(!document.getElementById('fit-vids-style')) {
+      // appendStyles: https://github.com/toddmotto/fluidvids/blob/master/dist/fluidvids.js
+      var head = document.head || document.getElementsByTagName('head')[0];
+      var css = '.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}';
+      var div = document.createElement("div");
+      div.innerHTML = '<p>x</p><style id="fit-vids-style">' + css + '</style>';
+      head.appendChild(div.childNodes[1]);
+    }
+
+    if ( options ) {
+      $.extend( settings, options );
+    }
+
+    return this.each(function(){
+      var selectors = [
+        'iframe[src*="player.vimeo.com"]',
+        'iframe[src*="youtube.com"]',
+        'iframe[src*="youtube-nocookie.com"]',
+        'iframe[src*="kickstarter.com"][src*="video.html"]',
+        'object',
+        'embed'
+      ];
+
+      if (settings.customSelector) {
+        selectors.push(settings.customSelector);
+      }
+
+      var ignoreList = '.fitvidsignore';
+
+      if(settings.ignore) {
+        ignoreList = ignoreList + ', ' + settings.ignore;
+      }
+
+      var $allVideos = $(this).find(selectors.join(','));
+      $allVideos = $allVideos.not('object object'); // SwfObj conflict patch
+      $allVideos = $allVideos.not(ignoreList); // Disable FitVids on this video.
+
+      $allVideos.each(function(){
+        var $this = $(this);
+        if($this.parents(ignoreList).length > 0) {
+          return; // Disable FitVids on this video.
+        }
+        if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
+        if ((!$this.css('height') && !$this.css('width')) && (isNaN($this.attr('height')) || isNaN($this.attr('width'))))
+        {
+          $this.attr('height', 9);
+          $this.attr('width', 16);
+        }
+        var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
+            width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
+            aspectRatio = height / width;
+        if(!$this.attr('name')){
+          var videoName = 'fitvid' + $.fn.fitVids._count;
+          $this.attr('name', videoName);
+          $.fn.fitVids._count++;
+        }
+        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+'%');
+        $this.removeAttr('height').removeAttr('width');
+      });
+    });
+  };
+  
+  // Internal counter for unique video names.
+  $.fn.fitVids._count = 0;
+  
+// Works with either jQuery or Zepto
+})( window.jQuery || window.Zepto );
+( function( $ ) {
+	"use strict";
+
+	// Set Fixed Header
+	var $cloneHeader = $('.main-header').clone().removeClass('main-header-original').addClass('main-header-clone').appendTo('body');
+	$(window).on('load scroll', function() {
+		var value = $(this).scrollTop();
+		if ( value > 300 ) {
+			$cloneHeader.addClass('main-header-clone-show');
+		} else {
+			$cloneHeader.removeClass('main-header-clone-show');
+			$('.main-header').removeClass('drawer-opened');
+		}
+	} );
+
+	// Set Drawer Menu
+	$('.drawer-hamburger').on('click', function() {
+		$(this).parent().parent().toggleClass('drawer-opened');
+	} );
+	$('.drawer-overlay').on('click',function() {
+		$('.main-header').removeClass('drawer-opened');
+	} );
+
+	// Set Smooth Scroll
+	$('a[href^=#]').click(function() {
+		var headerHight = $('.main-header-clone').outerHeight()+45;
+		var href = $(this).attr('href');
+		var target = $(href == '#' || href == '' ? 'html' : href);
+		var position = target.offset().top-headerHight;
+		$('html,body').animate({scrollTop:position}, 400, 'swing');
+	} );
+
+	// Set Back to Top
+	$(function() {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 300) {
+				$('.back-to-top').fadeIn();
+			} else {
+				$('.back-to-top').fadeOut();
+			}
+		});
+		$('.back-to-top').click(function () {
+			$('html,body').animate({scrollTop: 0}, 600, 'swing');
+			return false;
+		});
+	});
+
+	// Set Slick for Featured Posts.
+	if( 1 < $('.slick-item').length ) {
+		$('.featured-post').slick( {
+			centerMode: true,
+			centerPadding: '0',
+			dots: true,
+			mobileFirst: true,
+			slidesToShow: 1,
+		} );
+	}
+
+	// Set Stickyfill for Sticky Sidebar.
+	var $sticky_sidebar = $('#sticky-sidebar');
+	if( 0 < $sticky_sidebar.length ) {
+		Stickyfill.add($sticky_sidebar);
+	}
+
+	// Set Fitvids
+	$('.entry-content').fitVids();
+
+} )( jQuery );(function( $ ) {
+	"use strict";
+
+	function initMainNavigation( container ) {
+
+		// Add dropdown toggle that displays child menu items.
+		var dropdownToggle = $( '<button />', { 'class': 'dropdown-toggle', 'aria-expanded': false })
+			.append( $( '<span />', { 'class': 'screen-reader-text', text: businesspressScreenReaderText.expand }) );
+
+		container.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( dropdownToggle );
+
+		// Set the active submenu dropdown toggle button initial state.
+		container.find( '.current-menu-ancestor > button' )
+			.addClass( 'toggled-on' )
+			.attr( 'aria-expanded', 'true' )
+			.find( '.screen-reader-text' )
+			.text( businesspressScreenReaderText.collapse );
+		// Set the active submenu initial state.
+		container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
+
+		container.find( '.dropdown-toggle' ).click( function( e ) {
+			var _this = $( this ),
+				screenReaderSpan = _this.find( '.screen-reader-text' );
+
+			e.preventDefault();
+			_this.toggleClass( 'toggled-on' );
+			_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
+
+			_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+
+			screenReaderSpan.text( screenReaderSpan.text() === businesspressScreenReaderText.expand ? businesspressScreenReaderText.collapse : businesspressScreenReaderText.expand );
+		});
+	}
+
+	initMainNavigation( $( '.drawer-navigation .main-navigation' ) );
+})( jQuery );
+/**
+ * File skip-link-focus-fix.js.
+ *
+ * Helps with accessibility for keyboard only users.
+ *
+ * Learn more: https://git.io/vWdr2
+ */
+( function() {
+	var isIe = /(trident|msie)/i.test( navigator.userAgent );
+
+	if ( isIe && document.getElementById && window.addEventListener ) {
+		window.addEventListener( 'hashchange', function() {
+			var id = location.hash.substring( 1 ),
+				element;
+
+			if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
+				return;
+			}
+
+			element = document.getElementById( id );
+
+			if ( element ) {
+				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
+					element.tabIndex = -1;
+				}
+
+				element.focus();
+			}
+		}, false );
+	}
+} )();
